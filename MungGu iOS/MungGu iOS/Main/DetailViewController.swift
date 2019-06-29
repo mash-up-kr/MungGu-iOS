a85fd1811ea70c028a04e8edebe96e36eae0b624
AHNYELIM <ayl5033@gmail.com>
2019-06-30 00:57:53 +0900

    splitVIew displayMode 바꾸기

--- MungGu iOS/MungGu iOS/Main/DetailViewController.swift
+++ MungGu iOS/MungGu iOS/Main/DetailViewController.swift
@@ -8,23 +8,16 @@
 
 import UIKit
 
-class DetailViewController: UIViewController{
+class DetailViewController: UIViewController,UISplitViewControllerDelegate{
+    
+    @IBAction func expandBtn(_ sender: Any) {
+        self.splitViewController?.delegate = self
+        
+        let primaryHidden = splitViewController?.preferredDisplayMode ?? .primaryHidden
+        splitViewController?.preferredDisplayMode = primaryHidden == .allVisible ? .primaryHidden : .allVisible
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- MungGu iOS/MungGu iOS/Main/MySplitViewController.swift
+++ MungGu iOS/MungGu iOS/Main/MySplitViewController.swift
@@ -10,24 +10,8 @@ import UIKit
 
 class MySplitViewController: UISplitViewController {
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.preferredDisplayMode = UISplitViewController.DisplayMode.allVisible
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
