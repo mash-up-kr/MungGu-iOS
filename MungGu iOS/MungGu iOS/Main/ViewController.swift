f0eb302e2dfd27cf4d28aec4ec4177eef920a9ff
Daeyun Kim <kim.daeyun@navercorp.com>
2019-06-29 16:59:11 +0900

    Init Project

---
+++ MungGu iOS/MungGu iOS/Main/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MungGu iOS
+//
+//  Created by Daeyun Ethan on 29/06/2019.
+//  Copyright © 2019 Daeyun Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
