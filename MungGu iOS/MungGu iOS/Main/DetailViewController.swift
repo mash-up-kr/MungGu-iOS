9ee32ac571ec502790b5f705dbc4e63852a251fb
AHNYELIM <ayl5033@gmail.com>
2019-06-29 22:55:10 +0900

    spliView

---
+++ MungGu iOS/MungGu iOS/Main/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  MungGu iOS
+//
+//  Created by 안예림 on 29/06/2019.
+//  Copyright © 2019 Daeyun Ethan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MungGu iOS/MungGu iOS/Main/MySplitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MySplitViewController.swift
+//  MungGu iOS
+//
+//  Created by 안예림 on 29/06/2019.
+//  Copyright © 2019 Daeyun Ethan. All rights reserved.
+//
+
+import UIKit
+
+class MySplitViewController: UISplitViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.preferredDisplayMode = UISplitViewController.DisplayMode.allVisible
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
